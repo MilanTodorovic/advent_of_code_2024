7e3a2423898907bafcccd022cdcb25a2da5e1bbd
Milan Todorovic <todor-90@hotmail.com>
2024-12-10 15:19:07 +0100

    Added part two solution
    I didn't bother to refactor part one, but it is fast enough so who cares :D

--- Day_9/Day_9.py
+++ Day_9/Day_9.py
@@ -9,32 +9,46 @@ def open_file():
     return contents
 
 
-def decode_string():
-    pass
-
-
 def solve(part_two=False):
-    available_space = []
+    blocks = [] # (size, index) will be reversed for easier iteration
+    available_space = [] # (size, index)
     lst = []
-    dct = {}
-    _id = 0
     checksum = open_file()
+
     # if there is no empty space after the last block, it is NOT denoted
     if len(checksum) % 2 == 1:
         # adding hte trailing zero just to make things easier
         checksum += "0"
     
+    _id = 0
+    block_index = 0
+    space_index = 0
     for i in range(0,len(checksum),2):
-        block_id, block, empty_space = _id, checksum[i], checksum[i+1]
+        block_id, block_size, space_size = _id, int(checksum[i]), int(checksum[i+1])
+        block = [str(block_id)]*block_size
+        block_data = (block,block_index)
+        blocks.append(block_data)
+        lst.extend(block) # for part one
+        #lst.append(block)
+        if space_size:
+            # space index starts after a block: [9,9] at i=2 -> i=4 since [9,9] is index 2 and 3
+            space_index = block_index+block_size
+            available_space.append((space_size,space_index))
+            lst.extend(["."] * space_size)
+        else:
+            space_index = block_index+block_size
+        #print(f"{block_id}. block:{block_size} - space:{space_size}")
+        #print(f"\tBlock: {block} at {block_index}")
+        #print(f"\tSpace: {space_size} at {space_index}")
+        # new block starts right after the last empty space
+        block_index = space_index+space_size
+        #print("Block_index=", block_index)
+        # increment block_id
         _id += 1
-        # decoded string
-        dct[block_id] = str(block_id) * int(block) + "." * int(empty_space)
-        lst.extend([str(block_id)]*int(block))
-        lst.extend(["."] * int(empty_space))
 
     if not part_two:
     # fill empty space by moving numbers from the back to the front
-        for i in range(len(lst)-1,-1, -1):
+        for i in range(len(lst)-1,-1,-1):
             dot = lst.index(".")
             if dot > i:
                 # remove trailing dots
@@ -42,33 +56,61 @@ def solve(part_two=False):
                 break
             lst[dot], lst[i] = lst[i], lst[dot]
     else:
+        # Part two of the puzzle
         # fill empty space by moving whole blocks from the back to the front
-        j = 0
-        print("'.' found at:", lst.index(".", j))
-        dot = lst.index(".", j)
-        for k in range(dot, len(lst)):
-            while k<len(lst):
-                if lst[k] == ".":
-                    k += 1
+        #print("Before:", lst)
+        for block_data in blocks[::-1]:
+            block, block_index = block_data
+            block_size = len(block)
+            #print(f"Trying to move {block}")
+
+            for i, space in zip(range(len(available_space)), available_space):
+                space_size, space_index = space
+
+                if block_size <= space_size:
+                    #print(f"Block {block} is less then the space available at {space_index}:{space_index+space_size}")
+
+                    if block_index > space_index:
+                        #print(f"Block {block} index is {block_index} and space has index {space_index}")
+                        start = space_index
+                        end = space_index + block_size
+                        _start = block_index
+                        _end = block_index+block_size
+                        #print(f"Block {lst[_start:_end]} being moved to {lst[start:end]}")
+
+                        # swap positions 
+                        lst[start:end], lst[_start:_end] = lst[_start:_end], lst[start:end]
+                        remaining_space = space_size-block_size
+
+                        if remaining_space:
+                            available_space[i] = (remaining_space, space_index+block_size)
+                            #print("Remaing space", remaining_space, available_space[i])
+                        else:
+                            available_space.pop(i)
+                        break
+
+                    else:
+                        #print(f"Unable to move block {block} to {lst[space_index:space_size]}.")
+                        break
                 else:
-                    j = k + 1
-                    # available space at index i
-                    available_space.append((dot, j))
-                    break
-        print("Space:", available_space[:10])
+                    #print(f"Block {block} is greater than {lst[space_index:space_size]}")
+                    pass
+            #print(lst)
+        #print("After:", lst)
 
     # calculate new chechsum
     if not part_two:
         new_checksum = 0
         for i in range(len(lst)):
             new_checksum += i*int(lst[i])
-        print(new_checksum)
+        print("Part one checksum:", new_checksum)
     else:
-        #new_checksum = 0
-        #for i in range(len(lst)):
-        #    new_checksum += i*int(lst[i])
-        #print(new_checksum)
-        pass
+        new_checksum = 0
+        for i in range(len(lst)):
+            if lst[i] == ".":
+                continue
+            new_checksum += i*int(lst[i])
+        print("Part two checksum:", new_checksum)
 
 
 if __name__ == "__main__":

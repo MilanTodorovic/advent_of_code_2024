3b8433291ccf43013aa01f5d66fe192b3b0a0560
Milan Todorovic <todor-90@hotmail.com>
2024-12-11 12:42:31 +0100

    Cheesing my way to the top!
    Doesn't work YET

--- Day_10/Day_10.py
+++ Day_10/Day_10.py
@@ -1,113 +1,60 @@
 # Advent of Code 2024 - Day 10
-import sys
+from copy import deepcopy
+from time import time
 
 
-MAX_ROW = 0
-MAX_COL = 0
-VISITED_TILES = [] # eliminate paths that converge and then merge before 9
-PART_TWO = False
+def pre_calculate():
+	# the first 5 steps
+	pass
+
+def blink(stones, times=1):
+	# Rules:
+	#	0 -> 1
+	#	len("1000")%2 == 0 -> split into 2 stones int("10")|int("00") (no leading zeroes, so it becomes 0)
+	#	none of the above - stone_nr * 2024
+	# 	order is preserved!
+	for i in range(times):
+		_t = time()
+		tmp = deepcopy(stones)
+		split = 0
+		for j, stone in zip(range(len(tmp)),tmp):
+			if stone == "0":
+				stones[j+split] = "1"
+			elif len(stone)%2 == 0:
+				half = len(stone)//2
+				# removes leading zeroes
+				s1, s2 = str(int(stone[:half])), str(int(stone[half:]))
+				stones.pop(j+split)
+				stones = stones[:j+split] + [s1,s2] + stones[j+split:]
+				split += 1
+			else:
+				# pre-calculating the first 5 steps of every sub-10 number might be cheating
+				# using @cache might be as well
+				stones[j+split] = str(int(stone)*2024)
+		print("Blick",i+1, "in", (time()-_t)%60, "seconds.")
+	print(f"After {times} blinks:", stones)
+	print(f"No. of stones after {times} blinks:", len(stones))
 
 
 def open_file():
-    content = None
-    with open("./input.txt", "r") as file:
-        content = [line.strip() for line in file.readlines()]
-    return content
+	content = None
+	with open("./input.txt", "r") as file:
+		content = file.readline().strip()
+		content = [c.strip() for c in content.split(" ")]
+	return content
 
 
-def get_starting_locations(_map):
-    _locations = []
-    # no more zips and lens
-    i, j = 0, 0
-    for row in _map:
-        for col in row:
-            if col == "0":
-                _locations.append((i,j))
-            j+=1
-        j=0
-        i+=1
-    return _locations
-
-
-def get_valid_directions(_x, _y):
-    N, S, E, W = 0, 0, 0, 0
-
-    if _x < MAX_ROW-1:
-        S = _x + 1
-    if _x > 1 :
-        N = _x - 1
-    if _y < MAX_COL-1:
-        E= _y + 1
-    if _y > 1:
-        W = _y - 1
-
-    return [N,S,E,W]
-
-
-def traverse(_previous, _current, _map):
-    global VISITED_TILES
-
-    if _current[0] == MAX_ROW or _current[0] < 0:
-        return 0
-    if _current[1] == MAX_COL or _current[1] < 0:
-        return 0
-    
-    _t = _map[_current[0]][_current[1]]
-    # for some test inputs only
-    if _t == ".":
-        return 0
-    _t = int(_t)
-    
-    if _previous+1 == _t:
-        if _t == 9:
-            if _current not in VISITED_TILES:
-                if not PART_TWO:
-                    VISITED_TILES.append(_current)
-                #print("|------>Found the peak!|")
-                return 1
-            else:
-                return 0
-        else:
-            if _current not in VISITED_TILES:
-                if not PART_TWO:
-                    VISITED_TILES.append(_current)
-            else:
-                return 0
-            x, y = _current[0], _current[1]
-            return traverse(_t, (x+1,y), _map) + traverse(_t, (x-1,y), _map) + traverse(_t, (x,y+1), _map) + traverse(_t, (x,y-1), _map)
-    else:
-        return 0
-
-
-def find_path(_start, _map):
-    result = 0
-    result += traverse(-1, _start, _map)
-    #print(f"Path {_start} has a score of {result}.")
-    return result
-
-
-def solve(part_two=False):
-    global MAX_ROW, MAX_COL, VISITED_TILES, PART_TWO
-    
-    if part_two:
-        PART_TWO=True
-
-    topographic_map = open_file()
-    MAX_ROW = len(topographic_map)
-    MAX_COL = len(topographic_map[0])
-    starting_locations = get_starting_locations(topographic_map)
-
-    # A*? Breadth-first? Flood? Time will tell
-    sum_of_scores = 0
-    for start in starting_locations:
-        VISITED_TILES = []
-        #print(f"|Path: {start}          |")
-        sum_of_scores += find_path(start, topographic_map)
-        #print("|----------------------|")
-    print("The sum of all path scores is", sum_of_scores)
+def solve(blink_times=1, part_two=False):
+	content = open_file()
+	print(content)
+	pre_calculate()
+	# do the thing with dct look-ups
+	blink(content, blink_times)
 
 
 if __name__ == "__main__":
-    sys.setrecursionlimit(1000)
-    #solve() # 644
-    solve(True)
+	start = time()
+	solve(25)
+	end = time()
+	print("Time took to compute: ", (end-start)%60, " seconds.")
+	#solve(25, True)

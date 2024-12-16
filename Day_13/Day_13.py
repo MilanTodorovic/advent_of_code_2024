440c77c8d7452c848e6196e23e5c18ffe20a1145
Milan Todorovic <todor-90@hotmail.com>
2024-12-16 16:26:38 +0100

    Update Day_13.py

--- Day_13/Day_13.py
+++ Day_13/Day_13.py
@@ -33,6 +33,7 @@ def calculate(lst):
 	_A = lst[0]
 	_B = lst[1]
 	_P = lst[2]
+	goal = _P
 	print("Game:")
 	print(f"\tPx({_P[0]}) mod Ax({_A[0]}) {_P[0]%_A[0]}")
 	print(f"\tPy({_P[1]}) mod Ay({_A[1]}) {_P[1]%_A[1]}")
@@ -40,11 +41,40 @@ def calculate(lst):
 	print(f"\tPy({_P[1]}) mod By({_B[1]}) {_P[1]%_B[1]}")
 	print("-----------------------------")
 
+	if (_P[0] < _A[0] and _P[0] < _B[0]) or (_P[1] < _A[1] and _P[1] < _B[1]):
+		# Prize is unreachable
+		return (0,0)
+
+	# Pressing the A button costs x3 coins
+	#	if B*3 is moving a lot more, focus on pressing B
+	if _B[0]*3 > _A[0]:
+		_RB = _P[0]%_B[0], _P[1]%_B[1] 
+		while True:
+			# if there is a remained, subtrackt B from P and mod A
+			if _RB[0] or _RB[1]:
+				tmp = (_P[0]-_B[0], _P[1]-_B[1] )
+				_RA = tmp[0]%_A[0], tmp[1]%_A[1]
+				if _RA[0] or _RA[1]:
+					print(_RB, _RA, tmp)
+					continue
+				else:
+					# distance aka coins needed
+					# from A to the reduces P
+					# A = sqrt(pow((Px-Ax),2)+pow((Py-Ay),2))
+					# from reduced P to real P
+					# B = sqrt(pow((Px-P1x),2)+pow((Py-P1y),2))
+					print(_RB, _RA, tmp)
+					return (0,0)
+	else:
+		return (0,0)
+
+
 
 def solve():
 	open_file()
-	for k,v in GAMES.items():
-		calculate(v)
+	for k, v in GAMES.items():
+		r = calculate(v)
+		print(f"Game {k+1} needs a total of {sum(r)} coins.")
 
 
 if __name__ == "__main__":
